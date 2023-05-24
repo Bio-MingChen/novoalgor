d34861f99bc63ce8b68c3ec5665b500f5fb0455a
chenming <cm.bio@qq.com>
2023-05-25 07:04:08 +0800

    fix bug

--- get_top_n_marker.py
+++ get_top_n_marker.py
@@ -1,27 +1,32 @@
 import click
 import novotools.utils as tools
 
-#@click.command()
-#@click.argument("marker_file")
-#@click.option("--top_n",'-t',default=10,type=int,show_default=True,help="top n marker genes to retain")
-#@click.option("--ofile",'-o',default='filtered_marker_file.xls',show_default=True,help="output filename")
-def get_top_genes(marker_file,top_n,ofile):
+# @click.command()
+# @click.argument("marker_file")
+# @click.option("--top_n",'-t',default=10,type=int,show_default=True,help="top n marker genes to retain")
+# @click.option("--ofile",'-o',default='filtered_marker_file.xls',show_default=True,help="output filename")
+
+
+def get_top_genes(marker_file, top_n, ofile):
     """\b
     filter top n marker genes
     """
-    with open(marker_file,'r') as indata,\
-        open(ofile,'w') as odata:
+    with open(marker_file, 'r') as indata,\
+            open(ofile, 'w') as odata:
         title = indata.readline()
         odata.write("Celltype\tParent\tMarker\tNegative Marker\n")
         tp = tools.TitleParser(title)
         for line in indata:
             line_list = line.strip().split("\t")
-            celltype = tp.get_field(line_list,'Celltype',check=False)
-            marker = tp.get_field(line_list,'Marker',check=False)
-            parent = tp.get_field(line_list,'Parent',check=False) or celltype
-            negative_marker = tp.get_field(line_list,'Negative Marker',check=False) or ""
+            celltype = tp.get_field(line_list, 'Celltype', check=False)
+            marker = tp.get_field(line_list, 'Marker', check=False)
+            parent = tp.get_field(line_list, 'Parent', check=False) or celltype
+            negative_marker = tp.get_field(
+                line_list, 'Negative Marker', check=False) or ""
             filtered_marker = marker.split(",")[:top_n]
-            odata.write(f"{celltype}\t{parent}\t{','.join(filtered_marker)}\t{negative_marker}\n")
+            odata.write(
+                f"{celltype}\t{parent}\t{','.join(filtered_marker)}\t{negative_marker}\n")
+
 
 if __name__ == "__main__":
     get_top_genes()
--- utils.py
+++ utils.py
@@ -2,37 +2,44 @@ import re
 from collections import defaultdict
 import novotools.utils as tools
 
-def get_marker_meta(tag,line):
+
+def get_marker_meta(tag, line):
     tag_re = re.search(f'#{tag}:(.+)', line)
     if tag_re is None:
         raise Exception(f"{tag} is Required!")
     value = tag_re.group(1).strip().strip('"')
     return value
 
-def filter_genes_v2(adata,markers_dict,alarm_file):
+
+def filter_genes_v2(adata, markers_dict, alarm_file):
     alarms = []
     filtered_markers_dict = {}
-    for k,(parent,p,n) in markers_dict.items():
+    for k, (parent, p, n) in markers_dict.items():
         positive_filtered_genes = []
         negative_filtered_genes = []
-        for gene_list,result_list in zip((p,n),(positive_filtered_genes,negative_filtered_genes)):
+        for gene_list, result_list in zip((p, n), (positive_filtered_genes, negative_filtered_genes)):
             for g in gene_list:
                 if g not in adata.var_names:
                     print(f"{g} is not in this dataset, remove it!")
                     alarms.append(f"{g}不在数据集中，该marker基因在分析中将被移除！")
                 else:
                     result_list.append(g)
-        filtered_markers_dict[k] = (parent,positive_filtered_genes,negative_filtered_genes)
+        filtered_markers_dict[k] = (
+            parent, positive_filtered_genes, negative_filtered_genes)
     if alarm_file:
-        with open(alarm_file,'w') as odata:
+        with open(alarm_file, 'w') as odata:
             for alarm in alarms:
                 odata.write(alarm)
+    if not filtered_markers_dict:
+        raise Exception(
+            "No valid marker gene is found in dataset! Please check whether your input species is right!")
     return filtered_markers_dict
 
-def filter_genes(adata,markers_dict,alarm_file):
+
+def filter_genes(adata, markers_dict, alarm_file):
     alarms = []
     filtered_markers_dict = {}
-    for k,v in markers_dict.items():
+    for k, v in markers_dict.items():
         filtered_genes = []
         for g in v:
             if g not in adata.var_names:
@@ -42,24 +49,30 @@ def filter_genes(adata,markers_dict,alarm_file):
                 filtered_genes.append(g)
         filtered_markers_dict[k] = filtered_genes
     if alarm_file:
-        with open(alarm_file,'w') as odata:
+        with open(alarm_file, 'w') as odata:
             for alarm in alarms:
                 odata.write(alarm)
+
+    if not filtered_markers_dict:
+        raise Exception(
+            "No valid marker gene is found in dataset! Please check whether your input species is right!")
+
     return filtered_markers_dict
 
-def parse_markers_by_title(adata,marker_file,alarm_file=None):
+
+def parse_markers_by_title(adata, marker_file, alarm_file=None):
     """
     parse markers file by title name Celltype and Marker
     """
     markers_dict = defaultdict(list)
 
-    with open(marker_file,'r') as indata:
+    with open(marker_file, 'r') as indata:
         title = indata.readline()
         tp = tools.TitleParser(title)
         for line in indata:
             line_list = line.rstrip('\n').split('\t')
-            celltype = tp.get_field(line_list,"Celltype",check=False)
-            markers = tp.get_field(line_list,"Marker",check=False)
+            celltype = tp.get_field(line_list, "Celltype", check=False)
+            markers = tp.get_field(line_list, "Marker", check=False)
             if markers:
                 markers = [i.strip() for i in markers.split(",") if i.strip()]
             # if tp.have_title("Species"):
@@ -67,12 +80,13 @@ def parse_markers_by_title(adata,marker_file,alarm_file=None):
             #     if species in ["Mouse","mouse"]:
             #         markers = [i.capitalize() for i in markers]
 
-            markers_dict[celltype] = list(set(markers_dict[celltype] + markers))
-    
+            markers_dict[celltype] = list(
+                set(markers_dict[celltype] + markers))
 
-    return filter_genes(adata,markers_dict,alarm_file)
+    return filter_genes(adata, markers_dict, alarm_file)
 
-def parse_markers_type2(adata,marker_file):
+
+def parse_markers_type2(adata, marker_file):
     """
     parse markers and remove genes which is not exist in adata.var_names
     Args:
@@ -85,22 +99,23 @@ def parse_markers_type2(adata,marker_file):
     """
     markers_dict = {}
 
-    with open(marker_file,'r') as indata:
+    with open(marker_file, 'r') as indata:
         title = indata.readline()
         for line in indata:
             line_list = line.strip().split('\t')
             markers = [i.strip() for i in line_list[2].split(",")]
-            if line_list[0] in ["Mouse","mouse"]:
+            if line_list[0] in ["Mouse", "mouse"]:
                 markers = [i.capitalize() for i in markers]
             markers_dict[line_list[1]] = markers
 
-    return filter_genes(adata,markers_dict)
+    return filter_genes(adata, markers_dict)
 
-def parse_markers_type1(adata,marker_file):
+
+def parse_markers_type1(adata, marker_file):
     markers_dict = {}
     meta_dict = {}
 
-    with open(marker_file,'r') as indata:
+    with open(marker_file, 'r') as indata:
         for line in indata:
             if line.startswith("#name"):
                 meta_dict['name'] = get_marker_meta("name", line)
@@ -109,8 +124,8 @@ def parse_markers_type1(adata,marker_file):
             elif line.startswith("#tissue"):
                 meta_dict['tissue'] = get_marker_meta("tissue", line)
             else:
-                celltype,markers = line.strip().split("\t")
+                celltype, markers = line.strip().split("\t")
                 markers = [i.strip() for i in markers.split(",") if i]
                 markers_dict[celltype] = markers
-    
-    return filter_genes(adata,markers_dict)
+
+    return filter_genes(adata, markers_dict)
